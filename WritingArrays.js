ec0e707aa68746d26ebd55bc522f4d9447f02b6b
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-05-22 15:38:34 -0500

    Update WritingArrays.js

--- WritingArrays.js
+++ WritingArrays.js
@@ -1,7 +1,13 @@
+//first problem
 var testArr = [6,3,5,1,2,4]
 var sum = 0
 for (var i = 0; i <testArr.length; i++) {
     sum += testArr[i];
     console.log(testArr[i]);
     console.log(sum);
+}
+//new problem
+var testArr = [6,3,5,1,2,4]
+for (var i = 0; i <testArr.length; i++) {
+    console.log(testArr[i] * i)
 }
