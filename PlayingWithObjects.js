6bbfb6ceda4fea8b94e3f4de1f65451e347df9c5
robertjohnson1986 <83429757+robertjohnson1986@users.noreply.github.com>
2021-05-25 08:56:10 -0500

    Update PlayingWithObjects.js

--- PlayingWithObjects.js
+++ PlayingWithObjects.js
@@ -1,5 +1,3 @@
-// Author: Todd Enders
-
 var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
 
 // print John's age
@@ -8,7 +6,7 @@ console.log(users[1].age);
 // print the name of the first object
 console.log(users[0].name);
 
-// print the name and age of each user using a for loop
+// 3
 for (var i = 0; i < users.length; i++){
     console.log(users[i].name, "-", users[i].age);
 }
